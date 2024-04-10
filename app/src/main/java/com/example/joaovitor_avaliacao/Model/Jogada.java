8d82ae504c1c2d15c4406dce92b3c84dcdac13d9
JoaoVitor-dev <joaovitorreal@outlook.com>
2024-04-10 00:08:38 -0300

    Alterado classe para implementação da pontuação

---
+++ app/src/main/java/com/example/joaovitor_avaliacao/Model/Jogada.java
@@ -0,0 +1,9 @@
+package com.example.joaovitor_avaliacao.Model;
+
+public class Jogada
+{
+   private int ordem;
+   private int pontos;
+   private boolean ganhou;
+   private boolean ordemCrescente;
+}
--- app/src/main/java/com/example/joaovitor_avaliacao/Model/Pontuacao.java
+++
@@ -1,6 +0,0 @@
-package com.example.joaovitor_avaliacao.Model;
-
-public class Pontuacao
-{
-
-}
